3eae82f95ed09f0134c67bce8f82589b2ccc4ec4
Ranjit Kumar <mahtoji@gmail.com>
2022-01-29 23:38:09 -0800

    added columns as attributes of view form

--- v1.py
+++ v1.py
@@ -4,37 +4,78 @@ from sqlalchemy import Table
 from sqlalchemy import select
 from sqlalchemy import or_
 
-engine = create_engine("shillelagh://")
-connection = engine.connect()
+from flask import Flask, request, render_template, redirect
 
-table = Table('https://docs.google.com/spreadsheets/d/1RZstNkiNBTEBo8mJBwAYh29bQHOynEDxFb54hvHRsAc/edit#gid=1835168426', MetaData(bind=engine),autoload=True)
+#Example 4.2 Flask Webform dev
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-print(table.name)
-print(table.columns.keys())
+from pprint import pprint
 
-select_st = select([table]).where(table.c.Org == "CME")
-res = connection.execute(select_st)
+import viewform
 
-for _row in res:
-    print(_row)
+app = Flask(__name__)
+app.config['WTF_CSRF_ENABLED'] = False
 
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
+@app.route('/')
+def sql():
 
-rows = session.query(table.c.Org).all()
-for r in rows:
-    print(r)
+    engine = create_engine("shillelagh://")
+    connection = engine.connect()
 
-l = [v for v, in rows]
+    table = Table('https://docs.google.com/spreadsheets/d/1RZstNkiNBTEBo8mJBwAYh29bQHOynEDxFb54hvHRsAc/edit#gid=1835168426', MetaData(bind=engine),autoload=True)
 
-print(l)
+    print(table.name)
+    print(table.columns.keys())
 
-#Find all columns
-for c in table.columns:
-    rows = session.query(c).all()
-    #Find values from all columns
+    try:
+        rf = viewform.RowForm()
+    except Exception as e:
+        print("************GOT EXCEPTION************")
+        print(e)
+        print("************GOT EXCEPTION************")
+
+    for k in table.columns.keys():
+        k = k.replace(" ", "_")
+        k = k.replace(".", "_")
+        k = k.replace("(", "_")
+        k = k.replace(")", "_")
+        k = k.replace("/", "_")
+        k = k.replace("\\", "_")
+        #print(k)
+        rf.newAttr(k)
+    print("---------------------------------")
+    pprint(dir(rf))
+    pprint(vars(rf))
+    print("---------------------------------")
+    exit()
+
+    select_st = select([table]).where(table.c.Org == "CME")
+    res = connection.execute(select_st)
+
+    for _row in res:
+        print(_row)
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    rows = session.query(table.c.Org).all()
+    for r in rows:
+        print(r)
+
     l = [v for v, in rows]
+
     print(l)
 
+    #Find all columns
+    for c in table.columns:
+        rows = session.query(c).all()
+        #Find values from all columns
+        l = [v for v, in rows]
+        print(l)
+
+if __name__ == '__main__':
+    app.run()
